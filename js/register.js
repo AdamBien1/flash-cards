1bbbf2ec8f005cf512a4afd022243364d2068ff1
Adam Bien <adam.bien.97@gmail.com>
2020-08-27 20:52:43 +0200

    adding missing files

---
+++ js/register.js
@@ -0,0 +1,82 @@
+// Show input error message
+function showError(input, message) {
+  const small = input.parentElement.querySelector("small");
+  input.parentElement.className = "error";
+  small.innerText = message;
+  small.style.display = "block";
+}
+
+// Show input success message
+function showSuccess(input) {
+  input.parentElement.className = "success";
+  const small = input.parentElement.querySelector("small");
+  small.style.display = "none";
+}
+
+// Check email is valid
+function checkEmail(input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!re.test(input.value.trim())) {
+    showError(input, `${getFieldName(input)} is not a valid`);
+  } else {
+    showSuccess(input);
+  }
+}
+
+function getFieldName(field) {
+  return (
+    field.id.replace("sign-up-", "").charAt(0).toUpperCase() +
+    field.id.replace("sign-up-", "").slice(1)
+  );
+}
+
+// Check required fields
+function checkRequired(inputArr) {
+  inputArr.forEach((input) => {
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+// Check input length
+function checkLengh(inputArr) {
+  inputArr.forEach((inputObject) => {
+    if (inputObject.input.value.length < inputObject.min) {
+      showError(
+        inputObject.input,
+        `${getFieldName(inputObject.input)} must be at least ${inputObject.min}`
+      );
+    } else if (inputObject.input.value.length > inputObject.max) {
+      showError(
+        inputObject.input,
+        `${getFieldName(inputObject.input)} must be less than ${
+          inputObject.max
+        }`
+      );
+    } else {
+      showSuccess(inputObject.input);
+    }
+  });
+}
+
+// Check passwords match
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, "Passwords do not match");
+  }
+}
+
+function checkAll(username, email, password1, password2) {
+  checkRequired([username, email, password1, password2]);
+  checkLengh([
+    { input: username, min: 3, max: 15 },
+    { input: password1, min: 6, max: 25 },
+  ]);
+  checkEmail(email);
+  checkPasswordsMatch(password1, password2);
+}
+
+export { checkAll };
---
+++ js/view-cards.js
@@ -0,0 +1,87 @@
+// VIEW-CARDS.HTML VARS
+const cardsContainer = document.querySelector("#cards-container");
+const prevBtn = document.querySelector("#prev");
+const nextBtn = document.querySelector("#next");
+const currentEl = document.querySelector("#current");
+const questionEl = document.querySelector("#question");
+const answerEl = document.querySelector("#answer");
+let passIndex;
+
+let currentActiveCard = 0;
+const cardsEl = [];
+const cardsData = getCardsData();
+
+nextBtn.addEventListener("click", () => {
+  cardsEl[currentActiveCard].className = "card left";
+  currentActiveCard++;
+
+  if (currentActiveCard > cardsEl.length - 1) {
+    currentActiveCard = cardsEl.length - 1;
+  }
+
+  cardsEl[currentActiveCard].className = "card active";
+  updateCurrentText();
+});
+
+prevBtn.addEventListener("click", () => {
+  cardsEl[currentActiveCard].className = "card right";
+  currentActiveCard--;
+
+  if (currentActiveCard < 0) {
+    currentActiveCard = 0;
+  }
+
+  cardsEl[currentActiveCard].className = "card active";
+  updateCurrentText();
+});
+
+function updateCurrentText() {
+  currentEl.innerText = `${currentActiveCard + 1}/${cardsEl.length}`;
+}
+
+// Returns cards data from LOCAL STORAGE
+function getCardsData() {
+  const cards = JSON.parse(localStorage.getItem("cards"));
+  return cards === null ? [] : cards;
+}
+
+function createCards() {
+  cardsData[passIndex].body.forEach((data, index) => createCard(data, index));
+}
+
+function createCard(data, index) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  if (index === 0) {
+    card.classList.add("active");
+  }
+
+  card.innerHTML = `
+    <div class="inner-card">
+    <div class="inner-card-front">
+      <p>
+        ${data.question}
+      </p>
+    </div>
+    <div class="inner-card-back">
+      <p>
+        ${data.answer}
+      </p>
+    </div>
+  </div>
+    `;
+
+  card.addEventListener("click", () => card.classList.toggle("show-answer"));
+
+  cardsEl.push(card);
+  cardsContainer.appendChild(card);
+  updateCurrentText();
+}
+
+(function () {
+  passIndex = localStorage.getItem("passIndex");
+})();
+console.log(passIndex);
+console.log(cardsData);
+createCards();
